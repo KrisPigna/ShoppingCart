ccb6fa5639efb8d58581d9686532e786a0ac305d
KrisPigna <kris.pigna@gmail.com>
2016-11-15 17:57:02 -0500

    Added MainView class

---
+++ src/Views/MainView.java
@@ -0,0 +1,64 @@
+package Views;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import Inventory.Inventory;
+
+public class MainView extends JFrame {
+	public MainView(){
+		
+		//create instances of every view
+		loginView = new LoginView();
+		createView = new CreateAccountView();
+		inventoryView = new CustomerInventoryView();
+		
+		//add change listeners to each view
+		loginView.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent event) {
+				//determines which button was clicked, for event branching
+				JButton temp = (JButton) event.getSource();
+				if(temp.getText() == "Create Account") {
+					loginView.setVisible(false);
+					createView.setVisible(true);
+				}
+				if(temp.getText() == "Login") {
+					loginView.setVisible(false);
+					inventoryView.setVisible(true);
+				}
+			}
+		});
+		
+		createView.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent event) {
+				createView.setVisible(false);
+				loginView.setVisible(true);
+			}
+		});
+		
+		//Set layout and add each view to MainView frame; only LoginView is visible at first
+		this.setLayout(new FlowLayout());
+		this.add(loginView);
+		this.add(createView);
+		this.add(inventoryView);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		
+		MainView main = new MainView();
+	       
+		Inventory mainInventory = new Inventory();
+
+	}
+	
+	private LoginView loginView;
+	private CreateAccountView createView;
+	private CustomerInventoryView inventoryView;
+}

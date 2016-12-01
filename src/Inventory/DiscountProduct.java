f2b687abb21a848ccc97bf0140e638c41d950563
KrisPigna <kris.pigna@gmail.com>
2016-12-01 15:50:40 -0500

    Apparently forgot to actually push the DiscountProduct class last time..

---
+++ src/Inventory/DiscountProduct.java
@@ -0,0 +1,81 @@
+package Inventory;
+
+public class DiscountProduct implements GenericProduct {
+
+	@Override
+	public GenericProduct clone() throws CloneNotSupportedException {
+		return (GenericProduct) super.clone();
+	}
+
+	public DiscountProduct(GenericProduct prod, double disc){
+		product = prod;
+		discount = disc;
+	}
+
+	@Override
+	public String getName() {
+		return product.getName();
+	}
+	
+	@Override
+	public double getSellPrice() {
+		return product.getSellPrice() * discount;
+	}
+	
+	@Override
+	public double getWholesalePrice() {
+		return product.getWholesalePrice();
+	}
+	
+	@Override
+	public int getQty() {
+		return product.getQty();
+	}
+
+	@Override
+	public String getDescription() {
+		return product.getDescription();
+	}
+	
+	public void updateProduct(String n, int q, double p1, double p2, String d) {
+		product.setName(n);
+		product.setQty(q);
+		product.setSellPrice(p1);
+		product.setWholesalePrice(p2);
+		product.setDescription(d);
+	}
+	
+	@Override
+	public void setName(String n) {
+		product.setName(n);
+	}
+
+	@Override
+	public void setSellPrice(double p) {
+		product.setSellPrice(p);
+	}
+
+	@Override
+	public void setWholesalePrice(double p) {
+		product.setWholesalePrice(p);
+	}
+
+	@Override
+	public void setDescription(String d) {
+		product.setDescription(d);
+	}
+	
+	@Override
+	public void setQty(int key) {
+		product.setQty(key);
+	}
+	
+	public void setDiscount(double key) {
+		discount = key;
+	}
+	
+	private GenericProduct product;
+	private double discount;
+	private static final long serialVersionUID = 1L;
+
+}
